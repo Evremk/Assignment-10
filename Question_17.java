139d60045ecc3b0ccde0008e15ab3ea222ab44d5
Evremk <80709842+Evremk@users.noreply.github.com>
2021-05-03 23:09:09 +0200

    Add files via upload

---
+++ Question_17.java
@@ -0,0 +1,20 @@
+package assignment_10;
+
+public class Question_17 {
+
+	public static void main(String[] args) {
+		
+		printAlphabet('A','Z');	
+	
+		}
+		
+
+	public static void printAlphabet(char begin, char end  ) {
+		
+		for(char i = begin; i<=end;i++)
+		System.out.print(i);
+		}
+		
+	}
+
+
