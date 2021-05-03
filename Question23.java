f7fa543cf192c16eac295e729bb481d53a911f1e
Evremk <80709842+Evremk@users.noreply.github.com>
2021-05-03 03:22:52 +0200

    Add files via upload

---
+++ Question23.java
@@ -0,0 +1,24 @@
+package assignment_10;
+
+public class Question23 {
+
+	public static void main(String[] args) {
+		
+		
+		String word = "breadbutterbread";
+		int firstBread=word.indexOf("bread");
+		
+		if((word.contains("bread"))   && (word.substring(firstBread+6).contains("bread")) ) {
+				
+		String [] str1= word.split("bread");
+		
+		System.out.println(str1[1]);
+		
+			
+		}else {
+			System.out.println("nothing");
+		}
+		
+	}
+
+}
