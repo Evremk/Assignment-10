e42ac32157b368098581aee61920c9ca687a4b69
Evremk <80709842+Evremk@users.noreply.github.com>
2021-05-06 04:40:49 +0200

    Add files via upload

--- Question19.java
+++ Question19.java
@@ -4,11 +4,11 @@ public class Question19 {
 
 	public static void main(String[] args) {
 		
-		String word = "xxxxxxxx";
+		String word = "xxxjavaxxxxx";
 		String word2="java";
 		int counter=0;
 	
-		for(int i =0;i<word.length();i++) {
+		for(int i =0;i<word.length()-word2.length();i++) {
 		
 		if(word.substring(i).contains(word2)) {
 			counter++;
